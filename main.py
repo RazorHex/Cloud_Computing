4cd47c848327a8831e5232c846f3950d24df2250
Ahda Akmalul Ilmi <ahda043+GitHub@gmail.com>
2023-12-12 16:55:42 +0700

    Fixing filter

--- main.py
+++ main.py
@@ -1,19 +1,33 @@
 import streamlit as st
 import pandas as pd
 
-st.title('Hello world')
+st.title('Cloud Computing Kelompok 3')
 data = pd.read_csv('./data/diskon.csv', on_bad_lines='warn')
 
-input = st.text_input('Keyword')
+fashion = ['baju', 'celana', 'fashion','leather','jeans','gofood','shopeefood']
+kosmetik = ['liptint','lipstick','kosmetik','lipstick','makeup','make up']
+tiket= ['tiket', 'kendaraan', 'pesawat','bis','bus']
+makanan = ['kue','enak','lezat','sehat','madu']
 
-checks = [input in text for text in data['full_text']]
+selection = ['Fashion', 'Kosmetik', 'Tiket', 'Makanan', 'Lainnya']
 
-# [st.write(data['full_text'][i]) for i, x in enumerate(checks) if x == True]
+filter = st.multiselect('Filter', selection)
 
-for i, x in enumerate(checks):
-  if x == True:
-    st.write(data['full_text'][i])
-    st.divider()
+def detect_jenis_diskon(text):
+    if any(keyword in text for keyword in fashion):
+        return 'Fashion'
+    elif any(keyword in text for keyword in kosmetik):
+        return 'Kosmetik'
+    elif any(keyword in text for keyword in makanan):
+        return 'Makanan'
+    elif any(keyword in text for keyword in tiket):
+        return 'Tiket'
+    else:
+        return 'Lainnya'
 
+data['jenis_diskon'] = data['full_text'].str.lower().apply(detect_jenis_diskon)
 
-st.dataframe(data)
+filtered_data = data[data['jenis_diskon'].isin(filter)]
+count = filtered_data[filtered_data.columns[0]].count()
+st.write(f'{filtered_data[filtered_data.columns[0]].count()} Result')
+st.dataframe(filtered_data[['full_text', 'tweet_url']])
--- readme.md
+++ readme.md
@@ -1,4 +1,4 @@
-# Cloud Computing Kelompok
+# Cloud Computing Kelompok 3
 
 1. Virtual Environment : `python3 -m venv .venv`
 2. Aktifin virtual environment VS Code : `./.venv/Scripts/activate`
